280ebf1a3f4ed72fb314d784e8aead49229e0126
Joey <joeyecorbett@Gmail.com>
2020-03-28 14:34:45 +0000

    Auto stash before merge of "master" and "origin/master"

--- Project 2/Project 2/main.c
+++ Project 2/Project 2/main.c
@@ -5,8 +5,52 @@
  * Author : Aaron Naylor, Joseph Corbett
  */ 
 
-#include <avr/io.h>
+void sendmsg (char *s);
+void init_USART(void);
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+unsigned char qcntr = 0,sndcntr = 0;   /*indexes into the que*/
+unsigned char queue[50];       /*character queue*/
+
+/*message arrays*/
+char msg1[] = {"That was an a or an A."};
+char msg2[] = {"That was a b, not an a."};
+char msg3[] = {"That was neither b nor a."};
+	
+	
+
+int main(void)
+{
+	char ch;  /* character variable for received character*/
+
+	init_USART();
+
+
+	sei(); /*global interrupt enable */
+
+	while (1)
+	{
+		if (UCSR0A & (1<<RXC0)) /*check for character received*/
+		{
+			ch = UDR0;    /*get character sent from PC*/
+			switch (ch)
+			{
+				case 'A':
+				case 'a':
+				sendmsg(msg1); /*send first message*/
+				break;
+				case 'b':
+				sendmsg(msg2); /*send second message*/
+				break;
+				default:
+				sendmsg(msg3); /*send default message*/
+			}
+		}
+	}
+	return 1;
+}
 void init_adc() {
 
 }
@@ -15,7 +59,7 @@ void init_ports() {
 
 }
 
-void init_usart(){
+void init_USART(){
 	UCSR0A	= 0x00;
 	UCSR0B	= (1<<RXEN0) | (1<<TXEN0) | (1<<TXC0);  /*enable receiver, transmitter and transmit interrupt*/
 	UBRR0	= 103;  /*baud rate = 9600*/
@@ -33,14 +77,6 @@ void init_timer2() {
 	
 }
 
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
-}
-
 void sendmsg (char *s)
 {
 	qcntr = 0;    /*preset indices*/
@@ -54,4 +90,16 @@ void sendmsg (char *s)
 	UDR0 = queue[0];  /*send first character to start process*/
 }
 
+/********************************************************************************/
+/* Interrupt Service Routines													*/
+/********************************************************************************/
 
+/*this interrupt occurs whenever the */
+/*USART has completed sending a character*/
+
+ISR(USART_TX_vect)
+{
+	/*send next character and increment index*/
+	if (qcntr != sndcntr)
+	UDR0 = queue[sndcntr++];
+}
