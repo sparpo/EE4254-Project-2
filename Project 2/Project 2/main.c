6a3a25f78354f6caaee10b5cc89ee19d76006a46
Joey <joeyecorbett@Gmail.com>
2020-03-28 14:11:46 +0000

    Atmel Project setup

---
+++ Project 2/Project 2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project 2.c
+ *
+ * Created: 28/03/2020 14:11:00
+ * Author : Aaron Naylor, Joseph Corbett
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
