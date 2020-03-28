a589018dc3e67edce0dd9b353accd5aa9d400a33
AaronNaylor <58073562+AaronNaylor@users.noreply.github.com>
2020-03-28 14:27:59 +0000

    Update main.c

--- Project 2/Project 2/main.c
+++ Project 2/Project 2/main.c
@@ -11,7 +11,9 @@ void init_adc() {
 
 }
 void init_usart(){
-	
+	UCSR0A	= 0x00;
+	UCSR0B	= (1<<RXEN0) | (1<<TXEN0) | (1<<TXC0);  /*enable receiver, transmitter and transmit interrupt*/
+	UBRR0	= 103;  /*baud rate = 9600*/
 }
 
 void init_timer0() {
@@ -33,4 +35,18 @@ int main(void)
     {
     }
 }
+
+void sendmsg (char *s)
+{
+	qcntr = 0;    /*preset indices*/
+	sndcntr = 1;  /*set to one because first character already sent*/
+	
+	queue[qcntr++] = 0x0d;   /*put CRLF into the queue first*/
+	queue[qcntr++] = 0x0a;
+	while (*s)
+	queue[qcntr++] = *s++;   /*put characters into queue*/
+	
+	UDR0 = queue[0];  /*send first character to start process*/
+}
+
 
